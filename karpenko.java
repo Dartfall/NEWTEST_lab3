3d1f21b3f421c88c8603a29e8cc07e5c0951c7f0
Dartfall <megutron@mail.ru>
2022-03-05 10:17:35 +0300

    karpenko: 1

--- karpenko.java
+++ karpenko.java
@@ -2,6 +2,8 @@ package brokenrobotgame.model.events;
 
 import java.util.EventListener;
 
+//add new action
+
 public interface RobotActionListener extends EventListener {
     void robotMadeMove(RobotActionEvent e);
 }
