e76334c20d48b57bc73e04c51378017f255778ae
Danon2001 <bystriackoff@yandex.ru>
2022-03-05 10:26:58 +0300

    Daniil 3

--- bystryakov.java
+++ bystryakov.java
@@ -27,7 +27,7 @@ public class Humidity extends AbstractCharacteristic
     }
 
     @Override
-    public void update(EventObject event)
+    public void update(EventObject event,EventObject e )
     {
         if(value()>0)
             setValue(value()-1);
